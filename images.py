3904c49b8fc5d8565fcb626e75ee57dafeca8e5c
Rajesh Solanki <rajesh@wavemail.co.za>
2021-01-23 23:28:26 -0800

    adding 1st lot of files

---
+++ images.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageFilter
+
+img = Image.open('pics/rr.jpeg')
+#filtered_img = img.filter(ImageFilter.BLUR)
+#filtered_img = img.convert('L')
+#filtered_img.save("Grey.png",'png')
+#crooked_img = filtered_img.rotate(90)
+#filtered_img.show()
+#crooked_img.show()
+#resize_img = filtered_img.resize((300,300))  # must be a tuple
+#box = (100,100,400,400)
+#region = img.crop(box)
+#region.save("Cropped.png",'png')
+img.thumbnail((400,200))
+img.save('thumbnail.jpg')
+print(img.size)
